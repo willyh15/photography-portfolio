a1145fdd4b2f626a4d866a6e8ca3f83759f1a4ee
Will Harrison <willharrison2016@gmail.com>
2025-05-29 10:33:33 -0500

    Create +page.svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { Box, Heading, Text } from "@chakra-ui/svelte";
+</script>
+
+<Box p="4">
+  <Heading>Welcome to the Photography Portfolio</Heading>
+  <Text mt="2">This is the homepage.</Text>
+</Box>
