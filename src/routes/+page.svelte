13dc3485d76f6430d2680968a463613980f10f3b
Will Harrison <willharrison2016@gmail.com>
2025-05-29 10:40:41 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,31 @@
 <script>
-  import { Box, Heading, Text } from "@chakra-ui/svelte";
+  import { Box, Flex, Text, Heading } from "@chakra-ui/svelte";
 </script>
 
-<Box p="4">
-  <Heading>Welcome to the Photography Portfolio</Heading>
-  <Text mt="2">This is the homepage.</Text>
-</Box>
+<style>
+  .hero-bg {
+    background-image: url('/path-to-your-image.jpg');
+    background-size: cover;
+    background-position: center;
+    height: 100vh;
+    width: 100%;
+  }
+
+  .overlay {
+    background-color: rgba(0, 0, 0, 0.4);
+    width: 100%;
+    height: 100%;
+  }
+</style>
+
+<Flex class="hero-bg" justify="center" align="center">
+  <Box class="overlay" display="flex" alignItems="center" justifyContent="center" px="6" textAlign="center">
+    <Box color="white">
+      <Heading fontSize={{ base: "2xl", md: "4xl" }}>
+        Encouraging couples to relive<br />
+        moments, expressions,<br />
+        and feelings through narrative wedding films.
+      </Heading>
+    </Box>
+  </Box>
+</Flex>
