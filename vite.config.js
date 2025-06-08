748166e31c5559afeed68fcf7d3a64b0a2c0d59a
Will Harrison <willharrison2016@gmail.com>
2025-06-07 22:35:07 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,25 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 
-export default {
-  plugins: [sveltekit()]
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      $lib: path.resolve('./src/lib'),
+      $components: path.resolve('./src/components')
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [require('tailwindcss'), require('autoprefixer')]
+    }
+  },
+  server: {
+    fs: {
+      strict: false
+    }
+  }
 };
+
+export default config;
