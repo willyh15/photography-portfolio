f356d767cac8a855d0aca3c889d69930598a09d6
Will Harrison <willharrison2016@gmail.com>
2025-06-07 22:34:49 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,24 @@
 import adapter from '@sveltejs/adapter-vercel';
+import preprocess from 'svelte-preprocess';
 
-export default {
+const config = {
   kit: {
-    adapter: adapter()
-  }
+    adapter: adapter(),
+    alias: {
+      $lib: 'src/lib',
+      $components: 'src/components',
+    },
+    prerender: {
+      default: true,
+    },
+  },
+
+  preprocess: preprocess({
+    scss: {
+      includePaths: ['src'],
+    },
+    postcss: true,
+  }),
 };
+
+export default config;
