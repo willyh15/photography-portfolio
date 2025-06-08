ccefc765efdeb787524e6d216e6add4e2bafd1d9
Will Harrison <willharrison2016@gmail.com>
2025-06-08 01:56:44 -0500

    Create +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { ChakraProvider, ColorModeScript } from "@chakra-ui/svelte";
+  import theme from "../../theme/theme";
+</script>
+
+<ColorModeScript initialColorMode={theme.config.initialColorMode} />
+
+<ChakraProvider theme={theme}>
+  <slot />
+</ChakraProvider>
